6058292b1ecd460edac98dc4fe2a06f0d0f13f45
Jack Henley <jack.d.henley@gmail.com>
2015-01-21 18:54:55 -0500

    Added User model/controller

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/create'
+
+  get 'users/update'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
