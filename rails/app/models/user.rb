f66e763b432483e0dc57e51950c4339a64b69fb3
Jack Henley <jack.d.henley@gmail.com>
2015-01-21 19:25:31 -0500

    Added has many through to user for establishments, by visits and reviews

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
 	has_many :point_events
 	has_many :visits
 	has_many :reviews
+	has_many :establishments_visited, through: :visits, :source => :establishment
+	has_many :establishments_reviewed, through: :reviews, :source => :establishment
 end
